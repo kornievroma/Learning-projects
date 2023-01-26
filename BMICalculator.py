6ad210a32281839ee6ebe84076e4dc1d11037765
Gneida <korniev.roman@gmail.com>
2023-01-26 20:52:42 +0200

    add spaces to the code

--- BMICalculator.py
+++ BMICalculator.py
@@ -3,9 +3,9 @@ weight = int(input("enter your weight in kg: "))
 
 result = round(weight / height ** 2)
 
-if result <18.5:
+if result < 18.5:
     print(f"Your bmi is {result}, you are underweight")
-elif result <=25:
+elif result <= 25:
     print(f"Your bmi is {result}, you have a normal weight")
 elif result <= 30:
     print(f"Your bmi is {result}, you are overweight")
@@ -13,4 +13,3 @@ elif result <= 35:
     print(f"Your bmi is {result}, you are obese")
 else:
     print(f"Your bmi is {result}, you are clinically obese")
-

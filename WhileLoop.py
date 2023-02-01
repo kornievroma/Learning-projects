b867ee7bc7200bd8b85d95580b0f6a287e3229c3
Gneida <korniev.roman@gmail.com>
2023-02-01 22:54:02 +0200

    while loop practise

---
+++ WhileLoop.py
@@ -0,0 +1,16 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def pass1():
+    move ()
+    turn_left()
+    move ()
+    turn_right()
+    move ()
+    turn_right()
+    move()
+    turn_left()
+
+while at_goal() != True:
+    jump()
